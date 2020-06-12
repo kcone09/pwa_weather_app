e07b4ea6939199875042dc858a70986c7adc3dec
kcone <kcone09@gmail.com>
2020-06-12 22:23:31 +0200

    Updates

---
+++ precache-manifest.4d54ab8c7ca0f74aa1e635419543338e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5862fb474f145621d5909f087624ef",
+    "url": "/pwa_weather_app/index.html"
+  },
+  {
+    "revision": "9c495b8ef7a47a805142",
+    "url": "/pwa_weather_app/static/css/main.b09fb039.chunk.css"
+  },
+  {
+    "revision": "fe108db0aed4274efdb5",
+    "url": "/pwa_weather_app/static/js/2.4353b80b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa_weather_app/static/js/2.4353b80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c495b8ef7a47a805142",
+    "url": "/pwa_weather_app/static/js/main.7d0d2526.chunk.js"
+  },
+  {
+    "revision": "e1a5afe9bc20cf1537ee",
+    "url": "/pwa_weather_app/static/js/runtime-main.400f8e35.js"
+  },
+  {
+    "revision": "80e19d92838adbc8905385af48999139",
+    "url": "/pwa_weather_app/static/media/bg.80e19d92.jpg"
+  }
+]);
